d4731ce9c075174c4d1edbc0765f6356a2eed790
Cphayim <i@cphayim.me>
2022-02-25 08:13:46 +0800

    test(log): some test case

---
+++ packages/log/src/__tests__/index.spec.ts
@@ -0,0 +1,11 @@
+import { logger, figlet, boxen, colors, dedent } from '../index.js'
+
+describe('@vrn-deco/log -> index.ts', () => {
+  it('commonjs to esm named exports correctly', () => {
+    expect(logger).toBeDefined()
+    expect(figlet).toBeDefined()
+    expect(boxen).toBeDefined()
+    expect(colors).toBeDefined()
+    expect(dedent).toBeDefined()
+  })
+})
