ac10c6fe9a4076435463f6c492c1654c302a79d6
Hoyoe <hoyoe@cphayim.me>
2022-04-07 18:22:19 +0800

    feat(cli): prettify welcome logo of cli (#21)
    * feat(cli): prettify welcome logo of cli
    
    * chore: update logo image of readme.md

---
+++ packages/cli/src/__tests__/utils.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { gradient } from '../utils.js'
+
+describe('@vrn-deco/cli -> utils.ts', () => {
+  it('can be return a gradient string', () => {
+    expect(gradient('abcdefg')).toBeTypeOf('string')
+  })
+})
