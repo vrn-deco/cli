40e22b82103967da1aa029123a9b19ec49a721bb
Cphayim <i@cphayim.me>
2022-08-30 07:15:41 +0800

    chore(eslint): use ombro eslint preset

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@ombro/eslint-config-typescript'],
+  rules: {
+    'prefer-const': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+}
