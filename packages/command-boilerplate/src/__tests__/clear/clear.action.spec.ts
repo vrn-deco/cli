acff3dd86c3d8783bbef22f753f31fcb4ef40b77
Cphayim <i@cphayim.me>
2022-08-30 01:05:42 +0800

    chore(deps): update vitest version (#28)
    * chore(deps): update deps
    
    * test(cli): silent logger

--- packages/command-boilerplate/src/__tests__/clear/clear.action.spec.ts
+++ packages/command-boilerplate/src/__tests__/clear/clear.action.spec.ts
@@ -2,7 +2,10 @@ import fs from 'fs-extra'
 import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
 
 import { Command, runAction } from '@vrn-deco/cli-command'
+import { logger } from '@vrn-deco/cli-log'
 import { testShared } from '@vrn-deco/cli-shared'
+
+logger.setLevel('silent')
 
 // mock fs.existsSync and fs.removeSync
 const fsExistsSyncSpy = vi.spyOn(fs, 'existsSync').mockImplementation(() => true)
