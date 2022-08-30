c681027bca06dde7f60d07339c86f969a333f1cc
Cphayim <i@cphayim.me>
2022-08-30 18:51:13 +0800

    chore(eslint): use ombro eslint preset (#41)
    * chore(eslint): use ombro eslint preset
    
    * chore(eslint): update eslint rules
    
    * chore(deps): remove unless deps
    
    * chore(deps): fix lockfile
    
    Co-authored-by: Hoyoe <hoyoe@cphayim.me>

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@ombro/eslint-config-typescript'],
+  rules: {
+    'prefer-const': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+}
